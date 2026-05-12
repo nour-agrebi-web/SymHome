bff05e610650afd88399e569f317304608553090
Nour Agrebi <nragrebi@gmail.com>
2026-05-12 21:20:19 +0100

    update project

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RegistrationController extends AbstractController
 {
-    #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
-    {
-        return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
-        ]);
+    #[Route('/register', name: 'app_register')]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager,
+        UserRepository $userRepository
+    ): Response {
+        if ($request->isMethod('POST')) {
+            $nom = $request->request->get('nom');
+            $prenom = $request->request->get('prenom');
+            $email = $request->request->get('email');
+            $telephone = $request->request->get('telephone');
+            $adresse = $request->request->get('adresse');
+            $password = $request->request->get('password');
+
+            if ($userRepository->findOneBy(['email' => $email])) {
+                $this->addFlash('danger', 'Cet email existe déjà.');
+                return $this->redirectToRoute('app_register');
+            }
+
+            $user = new User();
+            $user->setNom($nom);
+            $user->setPrenom($prenom);
+            $user->setEmail($email);
+            $user->setTelephone($telephone);
+            $user->setAdresse($adresse);
+            $user->setRoles(['ROLE_USER']);
+            $user->setIsVerified(true);
+
+            $hashedPassword = $passwordHasher->hashPassword($user, $password);
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Compte créé avec succès. Vous pouvez vous connecter.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig');
     }
 }
