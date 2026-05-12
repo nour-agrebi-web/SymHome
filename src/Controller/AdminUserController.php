ae1d9520eeeb259c911127573c37caa1bb5934a8
Nour Agrebi <nragrebi@gmail.com>
2026-05-12 21:24:26 +0100

    update project

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin/users')]
 final class AdminUserController extends AbstractController
 {
-    #[Route('/admin/user', name: 'app_admin_user')]
-    public function index(): Response
+    #[Route('/', name: 'app_admin_user_index')]
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('admin_user/index.html.twig', [
-            'controller_name' => 'AdminUserController',
+            'users' => $userRepository->findBy([], [
+                'id' => 'DESC',
+            ]),
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_user_show')]
+    public function show(User $user): Response
+    {
+        return $this->render('admin_user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'app_admin_user_delete', methods: ['POST'])]
+    public function delete(User $user, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Utilisateur supprimé avec succès.');
+
+        return $this->redirectToRoute('app_admin_user_index');
     }
 }
