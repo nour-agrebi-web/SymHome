6734653d91dd380495b0668e51be0ebdddbea499
Ryhem Barhoumi <barhoumirihem4@gmail.com>
2026-05-12 21:42:33 +0100

    update project

--- src/Security/AuthAuthenticator.php
+++ src/Security/AuthAuthenticator.php
@@ -42,17 +42,14 @@ class AuthAuthenticator extends AbstractLoginFormAuthenticator
         );
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
-    {
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
-            return new RedirectResponse($targetPath);
-        }
-
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+{
+    if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+        return new RedirectResponse($targetPath);
     }
 
+    return new RedirectResponse($this->urlGenerator->generate('app_home'));
+}
     protected function getLoginUrl(Request $request): string
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
