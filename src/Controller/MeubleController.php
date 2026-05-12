89198096e800acdc10108a64b428304c80d284c2
Ryhem Barhoumi <barhoumirihem4@gmail.com>
2026-05-12 19:37:27 +0100

    move crud routes to admin

--- src/Controller/MeubleController.php
+++ src/Controller/MeubleController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/meuble')]
+#[Route('/admin/meuble')]
 final class MeubleController extends AbstractController
 {
     #[Route(name: 'app_meuble_index', methods: ['GET'])]
