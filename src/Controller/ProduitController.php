59b9fa691db50a0cf356be033176688fddc4fb2d
Nour Agrebi <nragrebi@gmail.com>
2026-05-12 20:28:52 +0100

    update project

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Meuble;
+use App\Repository\MeubleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
+    #[Route('/produits', name: 'app_produit_index')]
+    public function index(MeubleRepository $meubleRepository): Response
     {
         return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
+            'meubles' => $meubleRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/produit/{id}', name: 'app_produit_show')]
+    public function show(Meuble $meuble): Response
+    {
+        return $this->render('produit/show.html.twig', [
+            'meuble' => $meuble,
         ]);
     }
 }
