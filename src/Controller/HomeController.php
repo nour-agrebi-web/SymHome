de5a97c19ceaa8aae35198aa234d49697999e0ae
Ryhem Barhoumi <barhoumirihem4@gmail.com>
2026-05-12 22:20:50 +0100

    update project

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,24 +2,54 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
 use App\Repository\MeubleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(MeubleRepository $meubleRepository): Response
-    {
+    public function index(
+        Request $request,
+        MeubleRepository $meubleRepository,
+        CategorieRepository $categorieRepository
+    ): Response {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
-        $meubles = $meubleRepository->findAll();
+        $search = $request->query->get('search');
+        $categorieId = $request->query->get('categorie');
+
+        $queryBuilder = $meubleRepository->createQueryBuilder('m')
+            ->leftJoin('m.categorie', 'c')
+            ->addSelect('c');
+
+        if ($search) {
+            $queryBuilder
+                ->andWhere('m.nom LIKE :search')
+                ->setParameter('search', '%' . $search . '%');
+        }
+
+        if ($categorieId) {
+            $queryBuilder
+                ->andWhere('c.id = :categorieId')
+                ->setParameter('categorieId', $categorieId);
+        }
+
+        $meubles = $queryBuilder
+            ->orderBy('m.id', 'DESC')
+            ->getQuery()
+            ->getResult();
 
         return $this->render('home/index.html.twig', [
             'meubles' => $meubles,
+            'categories' => $categorieRepository->findAll(),
+            'search' => $search,
+            'categorieId' => $categorieId,
         ]);
     }
 }
