dfb952eddb022de4bbbef8ba744885801428ce08
Nour Agrebi <nragrebi@gmail.com>
2026-05-12 20:34:12 +0100

    update project

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,18 +3,50 @@
 namespace App\Controller;
 
 use App\Entity\Meuble;
+use App\Repository\CategorieRepository;
 use App\Repository\MeubleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProduitController extends AbstractController
 {
     #[Route('/produits', name: 'app_produit_index')]
-    public function index(MeubleRepository $meubleRepository): Response
-    {
+    public function index(
+        Request $request,
+        MeubleRepository $meubleRepository,
+        CategorieRepository $categorieRepository
+    ): Response {
+        $search = $request->query->get('search');
+        $categorieId = $request->query->get('categorie');
+
+        $queryBuilder = $meubleRepository->createQueryBuilder('m')
+            ->leftJoin('m.categorie', 'c')
+            ->addSelect('c');
+
+        if ($search) {
+            $queryBuilder
+                ->andWhere('m.nom LIKE :search')
+                ->setParameter('search', '%' . $search . '%');
+        }
+
+        if ($categorieId) {
+            $queryBuilder
+                ->andWhere('c.id = :categorieId')
+                ->setParameter('categorieId', $categorieId);
+        }
+
+        $meubles = $queryBuilder
+            ->orderBy('m.id', 'DESC')
+            ->getQuery()
+            ->getResult();
+
         return $this->render('produit/index.html.twig', [
-            'meubles' => $meubleRepository->findAll(),
+            'meubles' => $meubles,
+            'categories' => $categorieRepository->findAll(),
+            'search' => $search,
+            'categorieId' => $categorieId,
         ]);
     }
 
