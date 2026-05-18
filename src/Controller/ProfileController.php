4a00837fe5afdd02271028dbe8014bb9308be9a2
Agrebi Nour elhouda <nragrebi@gmail.com>
2026-05-18 20:07:40 +0100

    update project

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 final class ProfileController extends AbstractController
 {
@@ -14,5 +18,102 @@ final class ProfileController extends AbstractController
         $this->denyAccessUnlessGranted('ROLE_USER');
 
         return $this->render('profile/index.html.twig');
+    }
+
+    #[Route('/mon-compte/modifier', name: 'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $user = $this->getUser();
+
+        if ($request->isMethod('POST')) {
+            $user->setNom($request->request->get('nom'));
+            $user->setPrenom($request->request->get('prenom'));
+            $user->setTelephone($request->request->get('telephone'));
+            $user->setAdresse($request->request->get('adresse'));
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vos informations ont été modifiées avec succès.');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/mon-compte/mot-de-passe', name: 'app_profile_password')]
+    public function password(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $user = $this->getUser();
+
+        if ($request->isMethod('POST')) {
+            $currentPassword = $request->request->get('current_password');
+            $newPassword = $request->request->get('new_password');
+            $confirmPassword = $request->request->get('confirm_password');
+
+            if (!$passwordHasher->isPasswordValid($user, $currentPassword)) {
+                $this->addFlash('danger', 'Le mot de passe actuel est incorrect.');
+
+                return $this->redirectToRoute('app_profile_password');
+            }
+
+            if ($newPassword !== $confirmPassword) {
+                $this->addFlash('danger', 'Les deux nouveaux mots de passe ne sont pas identiques.');
+
+                return $this->redirectToRoute('app_profile_password');
+            }
+
+            if (strlen($newPassword) < 6) {
+                $this->addFlash('danger', 'Le nouveau mot de passe doit contenir au moins 6 caractères.');
+
+                return $this->redirectToRoute('app_profile_password');
+            }
+
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $newPassword)
+            );
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre mot de passe a été modifié avec succès.');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/password.html.twig');
+    }
+
+    #[Route('/mon-compte/supprimer', name: 'app_profile_delete', methods: ['POST'])]
+    public function delete(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        TokenStorageInterface $tokenStorage
+    ): Response {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        if (!$this->isCsrfTokenValid('delete_account', $request->request->get('_token'))) {
+            $this->addFlash('danger', 'Token invalide.');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        $user = $this->getUser();
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        $tokenStorage->setToken(null);
+        $request->getSession()->invalidate();
+
+        return $this->redirectToRoute('app_home');
     }
 }
